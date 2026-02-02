240fe75ec9399aa0c401effd2573b5b633020bdb
bcrowe306 <bcrowe306@gmail.com>
2026-02-01 23:38:26 -0500

    Scaffolding application

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/pug/v2"
+)
 
 func main() {
-	fmt.Fprintln(nil, "Hello, World!")
+	engine := pug.New("./views", ".pug")
+
+	app := fiber.New(fiber.Config{
+		Views: engine,
+	})
+
+	app.Static("/public", "./public")
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Render("pages/index", fiber.Map{})
+	})
+
+	app.Listen(":8080")
 }
---
+++ models.go
@@ -0,0 +1,9 @@
+package main
+
+// Profile Struct
+type Profile struct {
+	firstName   string
+	lastName    string
+	email       string
+	phoneNumber string
+}
